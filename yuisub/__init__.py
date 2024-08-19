3fd2796f942d6ef7baa5c99a48c972561d7f4ca7
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-08-20 01:27:27 +0800

    faet: add ad

--- yuisub/__init__.py
+++ yuisub/__init__.py
@@ -1,4 +1,4 @@
 from yuisub.bangumi import bangumi  # noqa: F401
 from yuisub.llm import Translator  # noqa: F401
 from yuisub.prompt import ORIGIN, ZH  # noqa: F401
-from yuisub.sub import bilingual, load, translate  # noqa: F401
+from yuisub.sub import advertisement, bilingual, load, translate  # noqa: F401
